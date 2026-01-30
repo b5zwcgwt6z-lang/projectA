a68d0f41f96b6b066dc1eb7052cac8086b18113a
b5zwcgwt6z-lang <b5zwcgwt6z@privaterelay.appleid.com>
2026-01-30 16:23:35 +0900

    이미지링크수정

--- js/challenge_swiper.js
+++ js/challenge_swiper.js
@@ -6,5 +6,26 @@ const swiper1 = new Swiper('.swiper1',{
   loop: true,
   slidesPerView:6,
   spaceBetween: 3,
+
   
+});
+
+// ringMenu button
+document.addEventListener('DOMContentLoaded', () => {
+    const ringItems = document.querySelector('.ringMenu-items');
+    const ringLeftBtn = document.querySelector('.ringMenu-buttonLeft');
+    const ringRightBtn = document.querySelector('.ringMenu-buttonRight');
+    const moveDistance = 360;
+
+    if (ringRightBtn && ringItems) {
+        ringRightBtn.addEventListener('click', () => {
+            ringItems.scrollBy({ left: moveDistance, behavior: 'smooth' });
+        });
+    }
+
+    if (ringLeftBtn && ringItems) {
+        ringLeftBtn.addEventListener('click', () => {
+            ringItems.scrollBy({ left: -moveDistance, behavior: 'smooth' });
+        });
+    }
 });
